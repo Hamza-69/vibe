5556dbe7f999ae7ecf39e5d479d108f0d431aaf7
Hamza El Rachdi <hamzarachidi04@gmail.com>
2025-07-06 16:36:38 +0300

    Update package dependencies, integrate TRPC and React Query for data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./Client";
+import { Suspense } from "react";
+
 const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({text: "hi"}))
   return (
-    <div>
-      hi
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
     
   )
 }
